d09608c40895d624ed411434a8aec94a424f8b3a
Raviteja Naidu <138877476+nick-0516@users.noreply.github.com>
2025-12-14 17:26:55 +0530

    Update user-service url

--- expense-service/src/main/java/com/smartexpense/expenseservice/client/UserClient.java
+++ expense-service/src/main/java/com/smartexpense/expenseservice/client/UserClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "http://localhost:8081")
+@FeignClient(name = "user-service", url = "http://localhost:8081") // this url might change when I host user-service on my old laptop
 public interface UserClient {
 
     @GetMapping("/api/users/by-email")
