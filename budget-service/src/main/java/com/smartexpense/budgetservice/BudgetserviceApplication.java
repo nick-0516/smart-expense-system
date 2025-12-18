739189384e54c9e953040226cd2fc893a7c5d78f
Raviteja <ravitejanaidu.edu.com>
2025-12-18 20:26:28 +0530

    Adding budget-service

---
+++ budget-service/src/main/java/com/smartexpense/budgetservice/BudgetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.smartexpense.budgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetserviceApplication.class, args);
+	}
+
+}
---
+++ budget-service/src/test/java/com/smartexpense/budgetservice/BudgetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartexpense.budgetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
