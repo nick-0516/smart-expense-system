d343dd814a4429f701c31e9acf577c6e05ce85c0
Raviteja <ravitejanaidu.edu.com>
2025-12-19 19:53:01 +0530

    Global exception handling

---
+++ budget-service/src/main/java/com/smartexpense/budgetservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.smartexpense.budgetservice.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFound(ResourceNotFoundException ex, HttpServletRequest req) {
+        return new ErrorResponse(Instant.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), req.getRequestURI());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse invalid(MethodArgumentNotValidException ex, HttpServletRequest req) {
+        var first = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst().map(e -> e.getField() + " " + e.getDefaultMessage())
+                .orElse("Validation failed");
+        return new ErrorResponse(Instant.now(), 400, first, req.getRequestURI());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleAccessDenied(AccessDeniedException ex, HttpServletRequest req) {
+        return new ErrorResponse(
+                Instant.now(),
+                HttpStatus.FORBIDDEN.value(),
+                ex.getMessage(),  //Not your budget, ***
+                req.getRequestURI()
+        );
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleRuntimeException (RuntimeException ex, HttpServletRequest req){
+        return new ErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                ex.getMessage(), //Budget already exists
+                req.getRequestURI()
+        );
+    }
+
+}
---
+++ budget-service/src/main/java/com/smartexpense/budgetservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.smartexpense.budgetservice.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
