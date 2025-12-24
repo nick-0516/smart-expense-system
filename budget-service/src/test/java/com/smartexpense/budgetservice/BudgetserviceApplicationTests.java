008a7be56643dcde7f326949a301eef18f203df4
Raviteja <ravitejanaidu.edu.com>
2025-12-24 21:44:28 +0530

    updated test config

--- budget-service/src/test/java/com/smartexpense/budgetservice/BudgetserviceApplicationTests.java
+++ budget-service/src/test/java/com/smartexpense/budgetservice/BudgetserviceApplicationTests.java
@@ -2,6 +2,7 @@ package com.smartexpense.budgetservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
