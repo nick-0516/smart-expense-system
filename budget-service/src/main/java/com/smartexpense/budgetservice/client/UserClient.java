a25aa2ba9bedf400757e8e55f46c593069ef9f70
Raviteja <ravitejanaidu.edu.com>
2026-06-04 17:41:33 +0530

    Updated url

--- budget-service/src/main/java/com/smartexpense/budgetservice/client/UserClient.java
+++ budget-service/src/main/java/com/smartexpense/budgetservice/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "user.smartexpense.in") // this url might change when I host user-service on my old laptop
+@FeignClient(name = "user-service", url = "http://localhost:8081") // this url might change when I host user-service on my old laptop
 public interface UserClient {
     @GetMapping("/api/users/by-email")
     UserResponse getByEmail(@RequestParam String email);
